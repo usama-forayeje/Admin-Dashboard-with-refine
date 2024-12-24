f1f7c110d56ac25850ad5cd512bf4600cb09b5e3
usama_forayaje <usamaforayaje03@gmail.com>
2024-12-25 00:37:42 +0600

    Add LatestActivities component and integrate into Home page; update imports and layout

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,7 +5,7 @@ import ProjectCardSkeleton from './skeleton/project-card'
 import LatestActivitiesSkeleton from './skeleton/latest-activities'
 import KanbanColumnSkeleton from './skeleton/kanban'
 import AccordionHeaderSkeleton from './skeleton/accordion-header'
-
+import LatestActivities from './home/latest-activities'
 export {UpcomingEvents, DealChart, UpcomingEventsSkeleton, ProjectCardSkeleton, LatestActivitiesSkeleton,
-    KanbanColumnSkeleton, AccordionHeaderSkeleton
+    KanbanColumnSkeleton, AccordionHeaderSkeleton, LatestActivities
 }
